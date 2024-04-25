42e413f6f9bd22c7985da89953eb236bc5cb2590
Vhas Vassell <vassellv@orcjamaica.com>
2024-04-25 18:13:02 -0500

    [ADDED] WebApplication

---
+++ kUwi_WebForm/Login_RegistrationScreen.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/KIWA.Master" CodeBehind="Login_RegistrationScreen.aspx.cs" Inherits="kUwi_WebForm.Login_RegistrationScreen" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Panel ID="Login" runat="server">
+        <asp:Label ID="LoginLabel" runat="server" Text="LOGIN"></asp:Label><br />
+        <asp:Label ID="UsernameLabel" runat="server" Text="Username: "></asp:Label> <asp:TextBox ID="UsernameTextBox" runat="server"></asp:TextBox><br /><br /><br />
+        <asp:Label ID="PasswordLabel" runat="server" Text="Password: "></asp:Label><asp:TextBox ID="PasswordTextBox" TextMode ="Password" runat="server"></asp:TextBox><br /><br />
+        <asp:LinkButton ID="RegistrationLinkButton" onclick="RegistrationLinkButton_Click" runat="server">Sign Up </asp:LinkButton>
+
+
+
+    </asp:Panel>
+    <asp:Panel ID="Registration" runat="server"></asp:Panel>
+</asp:Content>
+
---
+++ kUwi_WebForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("kUwi_WebForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("kUwi_WebForm")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5809716d-e032-4422-8e83-a37546690921")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
