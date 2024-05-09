da8aa9c9b57ff38538069ee9fd53b5a48bfdb546
Tona023 <uniquelylatona23@gmail.com>
2024-05-09 17:49:55 -0500

    [Created crednetials class]

---
+++ kUwi_WebForm/SecuredCredentials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kUwi_WebForm
+{
+    private int id;
+    private protected password_hashAndSalt;
+    private bool isStudent;
+    private bool isLecturer;
+
+    public class SecuredCredentials
+    {
+
+    }
+}
